7971abac695210149b4a1c2ec68fc92371338ffd
jackgray <jgrayau@gmail.com>
2021-08-14 14:36:43 -0400

    a simple shell script to wrap a shell script as an input argument inside a docker bash container, run it and destroy.Any data generated is saved to the current working directory on the local machine.

---
+++ containerize-script.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Containerizes and runs a shell script defined by CLI arg
+
+# TODO: add all scripts to github to be searched and pulled given appropriate input argument
+
+# Assign input arguments
+verbose=$2
+ 
+# SETUP
+CURRENT_DIR=$(pwd)
+PATH_TO_SCRIPT="$CURRENT_DIR/$1"
+CONTAINER_DIR=/container
+SCRIPT_NAME=$1
+BASE_IMAGE=bash:4.4
+# removes extension for file being containerized
+CONTAINER_NAME=$(echo "$1" | cut -f 1 -d '.')
+
+
+if $verbose
+then
+printf "\nLAUNCHING SCRIPT AS A CONTAINER\n*****************************************************\n\n"
+echo "Directory of script to be containerized:" 
+printf "%s\n\n" "$PATH_TO_SCRIPT"
+printf "using base image \"%s\"\n\n" $BASE_IMAGE
+fi
+
+if $verbose
+then
+printf "Name of container:\n'%s'\n\n" "$CONTAINER_NAME"
+fi
+
+install_drivers(){
+    docker plugin install --grant-all-permissions vieux/sshfs
+}
+
+create_volume(){
+    docker volume create --driver vieux/sshfs \
+
+}
+
+# make Dockerfile path stdin - leave no trace
+# IMPORTANT: buildkit MUST be used for file-less (stdin) context passing
+#            for CMD to execute on run
+docker_build(){
+    DOCKER_BUILDKIT=1 docker build --no-cache -t containerized:fileless -f- . <<EOF
+    FROM ubuntu
+    WORKDIR /app
+    COPY $SCRIPT_NAME .
+    RUN chmod +x $SCRIPT_NAME
+    CMD ["bash", "$SCRIPT_NAME"]
+EOF
+}
+
+docker_run(){
+    echo "Binding output from container volume to local working directory."
+    
+    docker run --rm -d \
+    --name shellname \
+    --mount type=bind,source="$(pwd)",target=/app \
+    containerized:fileless
+}
+
+containerize(){
+    #create_volume
+    docker_build
+    docker_run
+}
+containerize
